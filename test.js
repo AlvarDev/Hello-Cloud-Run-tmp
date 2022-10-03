c9bac87d33895dae4efb7a0f52a42012859b9dde
AlvarDev <alvardev@cloudadvocacyorg.joonix.net>
2022-10-03 18:26:37 -0300

    test and cloudbuild.yaml

--- test.js
+++ test.js
@@ -4,19 +4,18 @@ const request = require('supertest');
 const assert = require('assert');
 
 describe('Hello World', () => {
-  it('can respond to an HTTP request', (done) => {
-    console.log(`    - Requesting GET `);
-    request(app)
-	  .get('/')
-	  .expect(200)
-	  .end((e, res) => {
-		  should.not.exist(e);
-
-		assert.equal(res.status, '200');
-		assert.ok(
-    		  res.text.includes('Congratulations, you successfully deployed a container image to Cloud Run'),
-    		);
-		  done();
-	  });
-  });
+	it('can respond to an HTTP request with the correct title', (done) => {
+		console.log(`    - Requesting GET `);
+		request(app)
+			.get('/')
+			.expect(200)
+			.expect(res => {
+				assert.ok(
+					res.text.includes('Congratulations, you successfully deployed a container image to Cloud Run'));
+			})
+			.end((e,res) => {
+				should.not.exist(e);
+				done();
+			});
+	});
 });
